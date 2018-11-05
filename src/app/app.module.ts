2fa419b248c2a32e0a061a2833473710beaa033c
Daniel Estrada <isc.daniel.estrada@gmail.com>
2018-11-04 22:38:01 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
